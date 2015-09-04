dc33b20a9082d3c83226e3847cf81870127c0077
Enrico Tassi <gares@fettunta.org>
2015-09-04 13:56:04 +0200

    prover.sh: code factoring + separate build & run
    Now prover.sh and prover-elpi.sh accept (thanks to the common code in
    prover.lib.sh):
    --prepare-only       to just generate the runner.{mod,sig} and compile it
    --run-only           to just run the already compiled .lp file
    --module-name <name> to name runner.{mod,sig} differently (append <name>)
    
    In this way one can build everything and then run everything by iterating
    calls to ./prover.sh

---
+++ prover-elpi.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright (C) 2015 INRIA
+
+source prover.lib.sh
+
+parse_args "$@"
+init
+RUNNER_MOD_TARGET="${RUNNER_MOD_TARGET%%.mod}.elpi"
+
+if [ -z "$NO_PREPARE" ]; then
+
+  cleanup
+
+  
+  echo -e "module  ${MODNAME}.\\n" >> $RUNNER_MOD_TARGET
+  
+  if [ $ELEMENTS -lt 1 ]
+    then echo "Error: at least one certificate must be given!"
+    else
+    for (( i=0;i<$ELEMENTS;i++)); do
+          echo -e "accumulate ${ARGS[${i}]}.\\n" >> $RUNNER_MOD_TARGET
+    done
+  
+    `cat $RUNNER_MOD_BASE >> $RUNNER_MOD_TARGET`
+  fi
+fi
+
+export CMD="elpi -test ${MODNAME}.elpi"
+if [ -z "$NO_RUN" ]; then
+    cd $SRCDIR && \
+    source TJPATH && \
+    $CMD && \
+    rm -f $RUNNER_MOD_TARGET
+fi
+
--- prover.sh
+++ prover.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 # Copyright (C) 2015 INRIA
 
-BASEDIR="`pwd`"
-SRCDIR=$BASEDIR/src
-TOOLS=$SRCDIR/TOOLS
-RUNNER_MOD_BASE=$TOOLS/runner.mod.file
-RUNNER_SIG_BASE=$TOOLS/runner.sig.file
-RUNNER_MOD_TARGET=$SRCDIR/runner.mod
-RUNNER_SIG_TARGET=$SRCDIR/runner.sig
-export CMD="tjsim -b runner -s run."
+source prover.lib.sh
 
-ARGS=("$@")
-ELEMENTS=${#ARGS[@]}
+parse_args "$@"
+init
 
-rm -f $SRCDIR/kernel/lkf/lkf-kernel.lpo $SRCDIR/kernel/lkf/lkf-kernel.lp $SRCDIR/kernel/lkf/lkf-kernel.mod
-cp $TOOLS/kernel_debug/lkf-kernel.mod $SRCDIR/kernel/lkf/lkf-kernel.mod
+if [ -z "$NO_PREPARE" ]; then
 
-rm -f $RUNNER_MOD_TARGET
-rm -f $RUNNER_SIG_TARGET
+  cleanup
 
-echo -e "module runner.\\n" >> $RUNNER_MOD_TARGET
-echo -e "sig runner.\\n" >> $RUNNER_SIG_TARGET
+  echo -e "module ${MODNAME}.\\n" >> $RUNNER_MOD_TARGET
+  echo -e "sig ${MODNAME}.\\n" >> $RUNNER_SIG_TARGET
 
-if [ $ELEMENTS -ne 1 ]
-  then echo "Error: exactly one certificate must be given!"
-  else
-  for (( i=0;i<$ELEMENTS;i++)); do
-        echo -e "accum_sig ${ARGS[${i}]}.\\n" >> $RUNNER_SIG_TARGET
-        echo -e "accumulate ${ARGS[${i}]}.\\n" >> $RUNNER_MOD_TARGET
-  done
+  if [ $ELEMENTS -ne 1 ]
+    then echo "Error: exactly one certificate must be given!"
+    else
+    for (( i=0;i<$ELEMENTS;i++)); do
+          echo -e "accum_sig ${ARGS[${i}]}.\\n" >> $RUNNER_SIG_TARGET
+          echo -e "accumulate ${ARGS[${i}]}.\\n" >> $RUNNER_MOD_TARGET
+    done
+  
+    `cat $RUNNER_SIG_BASE >> $RUNNER_SIG_TARGET`
+    `cat $RUNNER_MOD_BASE >> $RUNNER_MOD_TARGET`
+  
+      cd $SRCDIR && \
+      make clean>/dev/null && \
+      make>/dev/null
+  fi
+fi
 
-  `cat $RUNNER_SIG_BASE >> $RUNNER_SIG_TARGET`
-  `cat $RUNNER_MOD_BASE >> $RUNNER_MOD_TARGET`
 
-  cd $SRCDIR && make clean>/dev/null && make>/dev/null && $CMD && rm -f $RUNNER_MOD_TARGET $RUNNER_SIG_TARGET
+export CMD="tjsim -b ${MODNAME} -s run."
+if [ -z "$NO_RUN" ]; then
+    cd $SRCDIR && \
+    $CMD && \
+    rm -f $RUNNER_MOD_TARGET $RUNNER_SIG_TARGET
 fi
